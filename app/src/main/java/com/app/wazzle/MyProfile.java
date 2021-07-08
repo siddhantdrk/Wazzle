46c3b98871f9c6c5113b0eaae460546260aeee0a
ashimsaini <ashimsaini37@gmail.com>
2021-07-08 21:52:53 +0530

    Edit profile

--- app/src/main/java/com/app/wazzle/MyProfile.java
+++ app/src/main/java/com/app/wazzle/MyProfile.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -13,6 +15,12 @@ public class MyProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
+        findViewById(R.id.iv_edit_profile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MyProfile.this, EditProfile.class));
+            }
+        });
 
         RecyclerView rv_songs_list = findViewById(R.id.rv_songs_list);
         MySongItemAdataper mySongItemAdataper = new MySongItemAdataper(this);
